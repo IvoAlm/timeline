0baf1dcb50e177b8c8bdbd31778ba82ffc424186
Ivo <ivodsalmeida@gmail.com>
2019-07-31 06:09:12 -0300

    change class name

--- app/EventType.php
+++ app/EventType.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Event_type extends Model
+class EventType extends Model
 {
     //
 }
--- app/ValueAdditionalField.php
+++ app/ValueAdditionalField.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Value_additional_field extends Model
+class ValueAdditionalField extends Model
 {
     //
 }
